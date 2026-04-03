20ff2f62e52b2243d6939e7fe329d835f7513571
kauafreireaureliano-dot <kauafreireaureliano@gmail.com>
2026-04-03 01:52:42 -0300

    fix: pixel via Cloudflare Function - sem cache, sempre atualizado

---
+++ functions/api/pixel-script.js
@@ -0,0 +1,14 @@
+export async function onRequestGet(context) {
+  const pixelId = context.env.META_PIXEL_ID || '';
+
+  const js = pixelId
+    ? `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','${pixelId}');fbq('track','PageView');`
+    : '/* meta pixel: META_PIXEL_ID not configured */';
+
+  return new Response(js, {
+    headers: {
+      'Content-Type': 'application/javascript',
+      'Cache-Control': 'no-store, no-cache, must-revalidate',
+    },
+  });
+}
